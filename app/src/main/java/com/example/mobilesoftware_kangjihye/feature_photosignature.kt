78a9dfdae8f3d5572cf6c79492df1b07fce44b17
kangjihye <tessa.kangjihye@gmail.com>
2023-12-18 03:54:36 +0900

    feature pages second update

--- app/src/main/java/com/example/mobilesoftware_kangjihye/feature_photosignature.kt
+++ app/src/main/java/com/example/mobilesoftware_kangjihye/feature_photosignature.kt
@@ -34,7 +34,7 @@ class feature_photosignature : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feature_photosagniture, container, false)
+        return inflater.inflate(R.layout.fragment_feature_photosignature, container, false)
     }
 
     companion object {
