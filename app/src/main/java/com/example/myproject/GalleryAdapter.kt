22703d271e1ed8e604c17b11843b5dd488aedcdd
thdusqpf35 <@naver.com>
2023-12-18 17:36:54 +0900

    Revert "갤러리,카메라 앱 연동"
    This reverts commit 958a31b87500643e3fd1824b8c66a0615ab6d195.

--- app/src/main/java/com/example/myproject/GalleryAdapter.kt
+++ app/src/main/java/com/example/myproject/GalleryAdapter.kt
@@ -1,71 +1,37 @@
 package com.example.myproject
 
-import android.graphics.Bitmap
+import android.app.ProgressDialog.show
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myproject.databinding.GalleryRecyclerviewBinding
-import java.util.Calendar
+import com.example.myproject.databinding.ItemRecyclerviewBinding
 
 class GalleryViewHolder(val binding: GalleryRecyclerviewBinding): RecyclerView.ViewHolder(binding.root)
 
-    class GalleryAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-        private val imagesList = mutableListOf<Bitmap>()
-        private val selectedDatesList = mutableListOf<Calendar>()
+class GalleryAdapter(val images2:MutableList<Int>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun getItemCount(): Int = images2.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+    = GalleryViewHolder(GalleryRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as GalleryViewHolder).binding
+        binding.itemImage.setImageResource(images2[position])
 
 
-        fun addImage(bitmap: Bitmap) {
-            imagesList.add(bitmap)
-            notifyItemInserted(imagesList.size - 1)
+        binding.itemTrashbtn.setOnClickListener{
+            AlertDialog.Builder(binding.root.context).run {
+                setMessage("정말 삭제하시겠습니까?")
+                setPositiveButton("아니요", null)
+                setNegativeButton("예", null)
+                show()
+            }
         }
 
-        fun addDate(year: Int, month: Int, dayOfMonth: Int) {
-            val selectedDate = Calendar.getInstance().apply {
-                set(Calendar.YEAR, year)
-                set(Calendar.MONTH, month)
-                set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            }
-                selectedDatesList.add(selectedDate)
-                notifyItemInserted(selectedDatesList.size - 1)
-            }
-            override fun onCreateViewHolder(
-                parent: ViewGroup,
-                viewType: Int
-            ): RecyclerView.ViewHolder = GalleryViewHolder(
-                GalleryRecyclerviewBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-            )
-
-            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-                val binding = (holder as GalleryViewHolder).binding
-                binding.itemImage.setImageBitmap(imagesList[position])
-                if(selectedDatesList.size > 0)
-                {
-                    val year = selectedDatesList[position].get(Calendar.YEAR)
-                    val month = selectedDatesList[position].get(Calendar.MONTH) + 1
-                    val dayOfMonth = selectedDatesList[position].get(Calendar.DAY_OF_MONTH)
-
-
-                    binding.itemDate.setText("$year.$month.$dayOfMonth")
-                }
-
-                binding.itemTrashbtn.setOnClickListener {
-                    AlertDialog.Builder(holder.itemView.context).run {
-                        setMessage("정말 삭제하시겠습니까?")
-                        setPositiveButton("아니요", null)
-                        setNegativeButton("예") { _, _ ->
-                            imagesList.removeAt(position)
-                            selectedDatesList.removeAt(position)
-                            notifyItemRemoved(position)
-                        }
-                        show()
-                    }
-                }
-            }
-            override fun getItemCount(): Int = imagesList.size
     }
 
+}
